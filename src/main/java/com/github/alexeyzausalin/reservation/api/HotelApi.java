36efa18a7f01fa46a5a736fe66dcdae09b8402db
Alexey Zausalin <alexeyzausalin@gmail.com>
2022-06-29 21:55:33 +0300

    Add ability to create hotel with tests

--- src/main/java/com/github/alexeyzausalin/reservation/api/HotelApi.java
+++ src/main/java/com/github/alexeyzausalin/reservation/api/HotelApi.java
@@ -1,11 +1,28 @@
 package com.github.alexeyzausalin.reservation.api;
 
+import net.minidev.json.JSONObject;
+import net.minidev.json.JSONValue;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/v1/hotels")
 public class HotelApi {
+
+    @PostMapping
+    public String create(@RequestBody String newHotel) {
+        Object obj = JSONValue.parse(newHotel);
+        JSONObject jsonObject = (JSONObject) obj;
+
+        String name = (String) jsonObject.getAsString("name");
+        if (name == null || name.isBlank()) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Not empty 'name' is required");
+        }
+
+        int id = 1;
+        return String.format("{\"id\":\"%d\"}", id);
+    }
 
     @GetMapping("/{id}")
     public String getHotel(@PathVariable Long id) {
